f47e72e5d7be80f0e502c9b24233ce96415e8ce9
liupeng <1098630129@qq.com>
2023-02-14 00:12:30 +0800

    2023第一次提交

--- drawVOC.py
+++ drawVOC.py
@@ -4,6 +4,7 @@ Created on 2022/11/20
 @author: liupeng
 
 """
+
 import os
 import xml.etree.cElementTree as et
 import matplotlib.pyplot as plt
@@ -45,7 +46,9 @@ for xmlFile in tqdm(files, desc='Processing'):
                 xmax = bndbox.find('xmax').text
                 ymax = bndbox.find('ymax').text
 
-                area = (int(ymax) - int(ymin)) * (int(xmax) - int(xmin))
+                # area = (int(ymax) - int(ymin)) * (int(xmax) - int(xmin))
+                area = int(ymax) - int(ymin) if int(ymax) - int(ymin) > int(xmax) - int(xmin) else (
+                        int(xmax) - int(xmin))
                 if area <= small:
                     smallArr_X.append(int(xmax) - int(xmin))
                     smallArr_Y.append(int(ymax) - int(ymin))
@@ -73,7 +76,7 @@ largeType = ax.scatter(largeArr_X, largeArr_Y, marker='.', c='darkblue')
 plt.legend((smallType, mediumType, largeType), ('small', 'medium', 'large'), loc='upper left', scatterpoints=1,
            labelspacing=1, edgecolor='black', framealpha=0.3, handletextpad=0)
 
-plt.grid()
+# plt.grid()
 plt.xlim((0, Axis_X_len))
 plt.ylim((0, Axis_Y_len))
 plt.xlabel('Width')
@@ -81,6 +84,11 @@ plt.ylabel('Height')
 
 ax1 = plt.gca()
 ax1.patch.set_facecolor("snow")  # 设置 ax1 区域背景颜色
-ax1.patch.set_alpha(1)  # 设置 ax1 区域背景颜色透明度
+ax1.patch.set_alpha(0)  # 设置 ax1 区域背景颜色透明度
 
 plt.show()
+
+print('small(%.0f%%' % (100 * round(len(smallArr_X) / (len(smallArr_X) + len(mediumArr_X) + len(largeArr_X)), 2)) + ')')
+print(
+    'medium(%.0f%%' % (100 * round(len(mediumArr_X) / (len(smallArr_X) + len(mediumArr_X) + len(largeArr_X)), 2)) + ')')
+print('large(%.0f%%' % (100 * round(len(largeArr_X) / (len(smallArr_X) + len(mediumArr_X) + len(largeArr_X)), 2)) + ')')
